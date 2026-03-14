1feca4cd825a04edf1368d95f5a1373fa1545aa9
Denis Manherz <denis@manherz.de>
2026-03-14 12:32:21 +0100

    asd

--- src/SolarSystemMain.jsx
+++ src/SolarSystemMain.jsx
@@ -1,11 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import {
-  Suspense,
-  useRef,
-  createContext,
-  useLayoutEffect,
-  useState,
-  useEffect,
+    Suspense,
+    useRef,
+    createContext,
+    useLayoutEffect,
+    useState,
+    useEffect,
 } from "react";
 
 import { ScreenOverlay } from "./planets/omnioverlay.jsx";
@@ -16,93 +16,92 @@ import { CustomCamera } from "./CustomCamera.jsx";
 export const MyContext = createContext();
 
 const SolarSystemScene = () => {
-  const controls = useRef();
-  let customData = useRef({});
-  const [pinfo, setPinfo] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [linePos, setLinePos] = useState(null);
+    const controls = useRef();
+    let customData = useRef({});
+    const [pinfo, setPinfo] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [linePos, setLinePos] = useState(null);
 
-  useLayoutEffect(() => {
-    const fetchPlanetInfo = async () => {
-      fetch(`http://192.168.1.136:8000/planetInfo`).then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        setLoading(true);
-        response
-          .json()
-          .then((data) => {
-            setPinfo(data);
-          })
-          .catch(() => {
-            setPinfo(null);
-          })
-          .finally(() => {
-            setLoading(false);
-          });
-      });
-    };
-    fetchPlanetInfo();
-    const fetchLinePos = async () => {
-      fetch(`http://192.168.1.136:8000/duration/line`).then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        setLoading(true);
-        response
-          .json()
-          .then((data) => {
-            
-            setLinePos(data);
-          })
-          .catch(() => {
-            setLinePos(null)
-          })
-          .finally(() => {
-            setLoading(false);
-          });
-      });
-    };
-    //fetchLinePos();
-    fetchPlanetInfo();
-  }, []);
+    useLayoutEffect(() => {
+        const fetchPlanetInfo = async () => {
+            fetch(`http://127.0.0.1:8002/planetInfo`).then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`,
+                    );
+                }
+                setLoading(true);
+                response
+                    .json()
+                    .then((data) => {
+                        setPinfo(data);
+                    })
+                    .catch(() => {
+                        setPinfo(null);
+                    })
+                    .finally(() => {
+                        setLoading(false);
+                    });
+            });
+        };
+        fetchPlanetInfo();
+        const fetchLinePos = async () => {
+            fetch(`http://127.0.0.1:8002/duration/line`).then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`,
+                    );
+                }
+                setLoading(true);
+                response
+                    .json()
+                    .then((data) => {
+                        setLinePos(data);
+                    })
+                    .catch(() => {
+                        setLinePos(null);
+                    })
+                    .finally(() => {
+                        setLoading(false);
+                    });
+            });
+        };
+        //fetchLinePos();
+        fetchPlanetInfo();
+    }, []);
 
-  useEffect(() => {
-    //console.log(progress);
-  });
+    useEffect(() => {
+        //console.log(progress);
+    });
 
-  return (
-    <Suspense fallback={<div className="loadingScreen">Suspense</div>}>
-      {!loading && (
-        <>
-          <MyContext.Provider value={{ controls, customData }}>
-            <ScreenOverlay planetInfo={pinfo} />
+    return (
+        <Suspense fallback={<div className="loadingScreen">Suspense</div>}>
+            {!loading && (
+                <>
+                    <MyContext.Provider value={{ controls, customData }}>
+                        <ScreenOverlay planetInfo={pinfo} />
 
-            {true && (
-              <Canvas
-                camera={{
-                  position: [0, 0, 100],
-                  fov: 75,
-                  near: 0.1,
-                  far: 10000000,
-                }}
-              >
-                {true && <CustomCamera />}
-                <Skybox />
-                <ambientLight intensity={0.025} />
-                <pointLight position={[0, 0, 0]} />
-                <SharedPlanetState planetData={pinfo} linePos={linePos}/>
-              </Canvas>
+                        {true && (
+                            <Canvas
+                                camera={{
+                                    position: [0, 0, 100],
+                                    fov: 75,
+                                    near: 0.1,
+                                    far: 10000000,
+                                }}
+                            >
+                                {true && <CustomCamera />}
+                                <Skybox />
+                                <ambientLight intensity={0.025} />
+                                <pointLight position={[0, 0, 0]} />
+                                <SharedPlanetState planetData={pinfo} linePos={linePos} />
+                            </Canvas>
+                        )}
+                    </MyContext.Provider>
+                </>
             )}
-          </MyContext.Provider>
-        </>
-      )}
-    </Suspense>
-  );
+        </Suspense>
+    );
 };
 
 export default SolarSystemScene;
